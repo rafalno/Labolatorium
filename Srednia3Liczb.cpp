c0843fcbe1f1b742edb1609c6f255a5247a5dfaa
Rafal <44160926+rafalno@users.noreply.github.com>
2022-11-06 23:05:34 +0100

    Add files via upload

---
+++ Srednia3Liczb.cpp
@@ -0,0 +1,21 @@
+// Zadanie 0.3
+
+#include <iostream>  
+
+using namespace std ; 
+
+
+int main () 
+{ 
+    float a , b , c , srednia ;
+
+cout << "Podaj 3 liczby zmiennoprzecinkowe: "<<endl; 
+cin >> a; 
+cin >> b;
+cin >> c;
+srednia=(a+b+c)/3;
+
+cout << " Srednia 3 podanych liczb wynosi:  " << srednia;
+
+return 0;
+}
