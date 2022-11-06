bc1d2e1fc4f0f9c2a524823b181cf5431e5405ad
Rafal <44160926+rafalno@users.noreply.github.com>
2022-11-06 23:02:08 +0100

    Add files via upload

---
+++ Celsjusz-Farenheit.cpp
@@ -0,0 +1,14 @@
+// Zadanie 0.2
+
+#include <iostream> 
+using namespace std;
+
+
+int main ()
+{
+float celsjus , farenheit ;
+
+cout << "Temperatura w stopniach Celsjusza: "; cin >> celsjus ; 
+farenheit=(celsjus*9/5)+32;
+cout << celsjus << " stopni celsjusza to " << farenheit << " stopni farenheita\n";
+}
