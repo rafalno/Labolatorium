fa8f146c0aef73aa0eda13135e77f0f5c5878ed2
Rafał Ogrodniczak <44160926+rafalno@users.noreply.github.com>
2022-11-17 07:49:13 +0100

    Add files via upload

---
+++ Zadanie_0.5.cpp
@@ -0,0 +1,23 @@
+// Zadanie 0.6 - 
+// autor: Rafal Ogrodniczak - WSB-NLU Student
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  float a, b;
+  cout << "Podaj dwa boki prostokata: "<< endl;
+  cin >> a;
+  cin >> b;
+  for (a>0 || b>0);
+    {cout <<a*b<< endl;}
+  else
+    {cout << "podane wartosci sa nieprawidlowe" << endl};
+
+
+  return 0;
+}
+
---
+++ Zadanie_0.6.cpp
@@ -0,0 +1,19 @@
+// Zadanie 0.6 - 
+//autor: Rafal Ogrodniczak - WSB-NLU Student
+
+#include <iostream>  
+
+using namespace std ; 
+
+float pole_prostokota (float a, float b)
+{
+    return a*b;
+}
+
+
+int main () 
+{ 
+cout << "Pole prostokate wynosi: "<<pole_prostokota(5.1, 4.0) <<endl; 
+return 0;
+}
+
